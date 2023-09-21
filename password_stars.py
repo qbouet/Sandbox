b3e782396370cc671e179fe2c3d2ec2422457ec8
qbouet <quentin.bouet@my.jcu.edu.au>
2023-09-21 12:03:36 +1000

    Correct password check program

--- password_stars.py
+++ password_stars.py
@@ -1,6 +1,11 @@
 
 def main():
+    min_length = 4
     password = str(input("Password: "))
+    while len(password) < min_length:
+        print("Password must be shorter than 4 characters")
+        password = str(input("Password: "))
+
     print(len(password) * '*')
 
 
